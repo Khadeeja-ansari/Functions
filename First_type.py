c1bf272f0b1db33da02cbd62ba03882f8726e4fc
Khadeeja-ansari <99644198+Khadeeja-ansari@users.noreply.github.com>
2022-03-14 18:47:12 +0530

    Add files via upload

---
+++ First_type.py
@@ -0,0 +1,10 @@
+# No arguments, no return value
+# input, calculation and output inside function
+# Addition of two numbers
+
+def Add():
+    a=int(input("Enter I number : "))
+    b=int(input("Enter II number :"))
+    c=a+b
+    print("Addition = ",c)
+Add()
