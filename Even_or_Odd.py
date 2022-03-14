c80d75286293e1264d00cf6dcf9147a889202062
Khadeeja-ansari <99644198+Khadeeja-ansari@users.noreply.github.com>
2022-03-14 18:48:03 +0530

    Add files via upload

---
+++ Even_or_Odd.py
@@ -0,0 +1,9 @@
+# Enter a number and check if it is even or odd
+
+def even():
+    n=int(input("Enter a number : "))
+    if n%2==0:
+        print("It is even number")
+    else:
+        print("It is odd number")
+even()
